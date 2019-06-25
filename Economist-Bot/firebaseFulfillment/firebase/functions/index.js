acd3444a7bcd597d89d8d8e4e0464e1ff38cb45b
Jessica Alonso <jessicaalonsodcl@gmail.com>
2019-06-25 18:42:03 -0300

    fixed error renda logic

--- Economist-Bot/firebaseFulfillment/firebase/functions/index.js
+++ Economist-Bot/firebaseFulfillment/firebase/functions/index.js
@@ -8,7 +8,7 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 const admin = require('firebase-admin');
 admin.initializeApp();
-var db = admin.firestore();
+let db = admin.firestore();
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
@@ -17,14 +17,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
 
   function welcome(agent) {
-    var welcomeRef = db.collection('Welcome');
+    let welcomeRef = db.collection('Welcome');
       return welcomeRef.get()
       .then((snapshot) => {
         let welcome = [];
           for(let doc of snapshot.docs){
             welcome.push(doc.data());
           }
-          var resposta = welcome[Math.floor(Math.random() * welcome.length)];
+          let resposta = welcome[Math.floor(Math.random() * welcome.length)];
           agent.add(resposta);
 
           return Promise.resolve(welcome);
@@ -45,40 +45,36 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     let divida = JSON.stringify(request.body.queryResult.outputContexts[0].parameters.question_yes_debts);
     let estadocivil;
 
-    console.log("SOLTEIRO ", solteiro);
-    console.log("DEPENDENTES", dependentes);
-    console.log("DIVIDA", divida);
-
-    if(solteiro === ""){
+    if(solteiro.length <= 2){
       estadocivil = 1;
     }else{ 
       estadocivil = 0;
     }
 
-    if(dependentes === ""){
+    if(dependentes.length <= 2){
       dependentes  = 0;
     }else{
       dependentes = 1;
     }
 
-    if (divida === ""){
+    if (divida.length <= 2){
       divida = 0;
     }else{ 
       divida = 1;
     }
 
-    var arrayRenda = [];
+    console.log(estadocivil,dependentes,divida);
+    let arrayRenda = [];
     arrayRenda.push(estadocivil);
     arrayRenda.push(dependentes);
     arrayRenda.push(divida);
 
-    console.log("ARRAY", arrayRenda);
     return arrayRenda;
   }
 
   function compare(arr1, arr2){
     if (arr1.length !== arr2.length) return false;
-    for (var i = 0, len = arr1.length; i < len; i++){
+    for (let i = 0, len = arr1.length; i < len; i++){
         if (arr1[i] !== arr2[i]){
             return false;
         }
@@ -91,7 +87,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     let analisarRenda;
     console.log(bool);
     if(bool === true){
-        console.log(bool, "TRUE ", regra);
         if(regra == "R1")  analisarRenda = true;
         else if (regra == "R2")  analisarRenda = true;
         else if (regra == "R3")  analisarRenda = true;
@@ -100,15 +95,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         else if (regra == "R6")  analisarRenda = true;
         else if (regra == "R7")  analisarRenda = false;
         else if (regra == "R8")  analisarRenda = true;
-        else console.log("ERROR");
       }
 
-    console.log("Analisar Renda", analisarRenda);
     return analisarRenda;
   }
 
   function verificarRegra(arr,arrR1,arrR2,arrR3,arrR4,arrR5,arrR6,arrR7,arrR8){
-    console.log("ARRAY 3", arr);
     let tipoDeRenda;
     let Regra1 = [];
     let Regra2 = [];
@@ -120,21 +112,13 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     let Regra8 = [];
 
     Regra1 = compare(arr, arrR1);
-    console.log("1 ", Regra1);
     Regra2 = compare(arr, arrR2);
-    console.log("2 ", Regra2);
     Regra3 = compare(arr, arrR3);
-    console.log("3 ", Regra3);
     Regra4 = compare(arr, arrR4);
-    console.log("4 ", Regra4);
     Regra5 = compare(arr, arrR5);
-    console.log("5 ", Regra5);
     Regra6 = compare(arr, arrR6);
-    console.log("6 ", Regra6);
     Regra7 = compare(arr, arrR7);
-    console.log("7 ", Regra7);
     Regra8 = compare(arr, arrR8);
-    console.log("8 ", Regra8);
 
     if(Regra1 === true) tipoDeRenda = verificarRenda(Regra1,"R1");
     if(Regra2 === true) tipoDeRenda = verificarRenda(Regra2,"R2");
@@ -145,42 +129,36 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     if(Regra7 === true) tipoDeRenda = verificarRenda(Regra7,"R7");
     if(Regra8 === true) tipoDeRenda = verificarRenda(Regra8,"R8");
 
-    console.log("Tipo de Renda", tipoDeRenda);
     return tipoDeRenda;
   }
 
   function validarRenda(agent){
-    var tipoDeRenda = false;
+    let tipoDeRenda = false;
     let arr = [];
     arr = regrasRenda(agent);
-    console.log("array 2", arr);
-    var arrR1 = [1,1,1];
-    var arrR2 = [1,1,0];
-    var arrR3 = [1,0,1];
-    var arrR4 = [1,0,0];
-    var arrR5 = [0,0,0];
-    var arrR6 = [0,1,1];
-    var arrR7 = [0,1,0];
-    var arrR8 = [0,0,1];
+    let arrR1 = [1,1,1];
+    let arrR2 = [1,1,0];
+    let arrR3 = [1,0,1];
+    let arrR4 = [1,0,0];
+    let arrR5 = [0,0,0];
+    let arrR6 = [0,1,1];
+    let arrR7 = [0,1,0];
+    let arrR8 = [0,0,1];
     
     let renda = "";
     tipoDeRenda = verificarRegra(arr,arrR1,arrR2,arrR3,arrR4,arrR5,arrR6,arrR7,arrR8);
 
-    console.log("mostra tipo de renda", tipoDeRenda);
-
     if(tipoDeRenda === true){
       renda = "Renda Comprometida";
-      console.log("Renda Comprometida");
     }else{
       renda = "Renda não Comprometida";
-      console.log("Renda não comprometida");
     }
 
     return (renda);
   }
 
   function tipoInvestidor(agent){
-    var rendacomprometida = validarRenda(agent);
+    let rendacomprometida = validarRenda(agent);
     console.log(rendacomprometida);
     return rendacomprometida;
   }
@@ -190,7 +168,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 
   function resultadoDois(agent){
-    var result = tipoInvestidor(agent);
+    let result = tipoInvestidor(agent);
     agent.add(result);
   }
 
